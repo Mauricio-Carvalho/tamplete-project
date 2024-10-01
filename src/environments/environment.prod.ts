685b87dab777965e0c34b0e8a1906d830fa2f815
Leonardo Cardoso Camilo <leolive_17@hotmail.com>
2024-09-30 22:08:12 -0300

    Update environment.prod.ts
    Atualizando a configuração para deploy em prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,18 @@
  * @license
  * Copyright Akveo. All Rights Reserved.
  * Licensed under the MIT License. See License.txt in the project root for license information.
- */
+ 
 export const environment = {
   production: true,
   baseUrl: 'http://localhost:80/v1',
+};
+*/
+
+export const environment = {
+  production: true,
+  baseUrl: "https://api-msvtecnologia.com.br",  // URL base da API
+  tokenAllowedDomains: [/api\-msvtecnologia\.com\.br/],  // Domínio onde a API está hospedada
+  tokenDisallowedRoutes: [/\/oauth2\/token/],  // Rotas que não devem enviar tokens (como a de autenticação)
+  oauthCallbackUrl: "https://msvtecnologia.com.br/authorized",  // URL de callback após autenticação
+  logoutRedirectToUrl: "https://msvtecnologia.com.br"  // URL para redirecionamento após logout
 };
