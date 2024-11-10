4233dc184efb6248e541c0b0a2799375dc6bf822
MAURICIO APARECIDO DE CARVALHO <m.decarvalho@dxc.com>
2024-11-10 18:44:38 -0300

    :ok: : temperature

--- src/app/pages/dashboard/temperature/temperature.component.ts
+++ src/app/pages/dashboard/temperature/temperature.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnDestroy } from '@angular/core';
 import { NbThemeService } from '@nebular/theme';
-import { Temperature, TemperatureHumidityData } from '../../../@core/data/temperature-humidity';
+import { TemperatureHumidityData } from '../../../@core/data/temperature-humidity';
 import { takeWhile } from 'rxjs/operators';
-import { forkJoin } from 'rxjs';
+import { AnalyticalService } from '../analytical.service';
+import { TruckFuelData } from '../../../shared/model/truckFuelData';
 
 @Component({
   selector: 'ngx-temperature',
@@ -13,37 +14,53 @@ export class TemperatureComponent implements OnDestroy {
 
   private alive = true;
 
-  temperatureData: Temperature;
+  truckFuelData: TruckFuelData[] = [];
   temperature: number;
-  temperatureOff = false;
-  temperatureMode = 'cool';
-
-  humidityData: Temperature;
   humidity: number;
-  humidityOff = false;
-  humidityMode = 'heat';
-
   theme: any;
   themeSubscription: any;
 
+  // Definindo as propriedades específicas para exibição
+  idTruck: string;
+  model: string;
+  countFuelS10: number;
+  fuelPercentageS10: number;
+  countFuelS500: number;
+  fuelPercentageS500: number;
+  nfCombS10: string;
+  nfComb500: string;
+
   constructor(private themeService: NbThemeService,
-              private temperatureHumidityService: TemperatureHumidityData) {
+              private temperatureHumidityService: TemperatureHumidityData,
+              private analyticalService: AnalyticalService) {
+    this.getTruckFuelData();
     this.themeService.getJsTheme()
       .pipe(takeWhile(() => this.alive))
       .subscribe(config => {
-      this.theme = config.variables.temperature;
-    });
+        this.theme = config.variables.temperature;
+      });
+  }
 
-    forkJoin(
-      this.temperatureHumidityService.getTemperatureData(),
-      this.temperatureHumidityService.getHumidityData(),
-    )
-      .subscribe(([temperatureData, humidityData]: [Temperature, Temperature]) => {
-        this.temperatureData = temperatureData;
-        this.temperature = this.temperatureData.value;
+  // Buscar dados de combustível dos caminhões
+  getTruckFuelData() {
+    this.analyticalService.getTruckFuelData()
+      .pipe(takeWhile(() => this.alive))
+      .subscribe((data: TruckFuelData[]) => {
+        this.truckFuelData = data;
+        if (data.length > 0) {
+          const truck = data[0]; // Exemplo com o primeiro caminhão, adaptar conforme necessário
+          this.idTruck = truck.idTruck;
+          this.model = truck.model;
+          this.countFuelS10 = truck.countFuelS10;
+          this.fuelPercentageS10 = truck.fuelPercentageS10;
+          this.countFuelS500 = truck.countFuelS500;
+          this.fuelPercentageS500 = truck.fuelPercentageS500;
+          this.nfCombS10 = truck.nfCombS10;
+          this.nfComb500 = truck.nfComb500;
 
-        this.humidityData = humidityData;
-        this.humidity = this.humidityData.value;
+          this.temperature = truck.fuelPercentageS10;
+          this.humidity = truck.fuelPercentageS500;
+        }
       });
   }
 
