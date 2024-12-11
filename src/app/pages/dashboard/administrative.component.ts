80493e13b77337da5c307cd21c90b076515c4749
MAURICIO APARECIDO DE CARVALHO <m.decarvalho@dxc.com>
2024-12-11 16:00:37 -0300

    powerBI

--- src/app/pages/dashboard/administrative.component.ts
+++ src/app/pages/dashboard/administrative.component.ts
@@ -1,11 +1,8 @@
 
 import {Component, OnDestroy} from '@angular/core';
 import { NbComponentStatus, NbDialogService, NbToastrService } from '@nebular/theme';
-import { AnalyticalService } from './analytical.service';
 import { TranslateService } from '@ngx-translate/core';
 import { takeWhile } from 'rxjs';
-import { TruckFuelData } from '../../shared/model/truckFuelData';
-import { MachineFilledPercentageDto } from '../../shared/model/machineFilledPercentageDto';
 
 
 @Component({
@@ -14,132 +11,8 @@ import { MachineFilledPercentageDto } from '../../shared/model/machineFilledPerc
   templateUrl: './administrative.component.html',
 })
 export class AdministrativeComponent {
-
-
-  amountOperator: number = 0;
-  amountMachine: number = 0;
-  amountFuelS10: number = 0;
-  amountFuelS500: number = 0;
-
-  machineNames: string[] = [];
-  fuelAmounts: number[] = [];
-  maxFuelAmount: number = 0;
-  fuelTypes: string[] = [];
-  showFuelMachines = false;
-
-
-  terminals = [];
-  queues = [];
-  isSelected = false;
-
-  truckFuelData: TruckFuelData[] = [];
-
-  machineFuelData: MachineFilledPercentageDto[] = [];
-  private alive = true;
-
   constructor(
-              private analyticalService: AnalyticalService,
               private toastrService: NbToastrService,
-              private translate: TranslateService) {
-    this.countOperator();
-    this.countMachine();
-    this.countFuel();
-    this.fetchTruckFuelData();
-    this.fetchMachineFuelData(null, null);
-  }
+              private translate: TranslateService) {}
 
-  countOperator() {
-    this.analyticalService.countOperator()
-      .then((amountOperator) => {
-        this.amountOperator = amountOperator['count'];
-      })
-      .catch((response) => {
-        this.toastrService.danger(this.translate.instant('toastr.create.error.message'), this.translate.instant('toastr.create.error.title'));
-      });
-  }
-
-  generateFakeMachineFuelData() {
-    this.machineNames = ['Máquina 01', 'Máquina 02', 'Máquina 03', 'Máquina 04', 'Máquina 05'];
-    this.fuelAmounts = this.machineNames.map(() => Math.floor(Math.random() * 500));
-
-    const maxFuel = Math.max(...this.fuelAmounts); // Maior valor em fuelAmounts
-    this.maxFuelAmount = maxFuel + 100; // Soma 500 ao maior valor
-    console.log('maxFuelAmount: ' + this.maxFuelAmount)
-  }
-
-  countMachine() {
-    this.analyticalService.countMachine()
-      .then((amountMachine) => {
-        this.amountMachine = amountMachine['count'];
-      })
-      .catch((response) => {
-        this.toastrService.danger(this.translate.instant('toastr.create.error.message'), this.translate.instant('toastr.create.error.title'));
-      });
-  }
-
-  countFuel() {
-    this.analyticalService.countFuel()
-      .then((fuelAll) => {
-        this.amountFuelS10 = fuelAll['totalS10'];
-        this.amountFuelS500 = fuelAll['totalS500'];
-      })
-      .catch((response) => {
-        this.toastrService.danger(this.translate.instant('toastr.create.error.message'), this.translate.instant('toastr.create.error.title'));
-      });
-  }
-
-  fetchTruckFuelData() {
-    this.analyticalService.getTruckFuelData()
-      .pipe(takeWhile(() => this.alive))
-      .subscribe((data: TruckFuelData[]) => {
-        this.truckFuelData = data;
-      });
-  }
-
-
-  // Função para buscar os dados de abastecimento de máquinas
-  fetchMachineFuelData(startDate: string, endDate: string) {
-    this.showFuelMachines = false;
-    this.analyticalService.getMachineFuelData(startDate, endDate)
-      .pipe(takeWhile(() => this.alive))
-      .subscribe(
-        (data: MachineFilledPercentageDto[]) => {
-          // Extrai os nomes das máquinas
-          this.machineNames = data.map(machine => machine.name);
-
-          // Extrai os valores abastecidos
-          this.fuelAmounts = data.map(machine => machine.amountFilled);
-
-          // Extrai os tipos de combustível
-          this.fuelTypes = data.map(machine => machine.comb);
-
-          // Calcula o maior valor abastecido + 100
-          const maxFuel = Math.max(...this.fuelAmounts);
-          this.maxFuelAmount = maxFuel + 100;
-
-          console.log('machineNames:', this.machineNames);
-          console.log('fuelAmounts:', this.fuelAmounts);
-          console.log('fuelTypes:', this.fuelTypes);
-          console.log('maxFuelAmount:', this.maxFuelAmount);
-          this.showFuelMachines = true;
-        },
-        (error) => {
-          // Trata o erro e exibe uma mensagem ao usuário
-          this.toastrService.danger(
-            this.translate.instant('toastr.fetch.error.message'),
-            this.translate.instant('toastr.fetch.error.title')
-          );
-        }
-      );
-
-  }
-
-
-  onDeleteConfirm(event): void {
-    if (window.confirm('Are you sure you want to delete?')) {
-      event.confirm.resolve();
-    } else {
-      event.confirm.reject();
-    }
-  }
 }
--- src/app/pages/dashboard/administrative.module.ts
+++ src/app/pages/dashboard/administrative.module.ts
@@ -21,23 +21,6 @@ import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 import { NbDatepickerModule } from '@nebular/theme';
 
-import { TrafficRevealCardComponent } from './traffic-reveal-card/traffic-reveal-card.component';
-import { StatusCardComponent } from './status-card/status-card.component';
-import { TrafficBarChartComponent } from './traffic-reveal-card/back-side/traffic-bar-chart.component';
-import { TrafficFrontCardComponent } from './traffic-reveal-card/front-side/traffic-front-card.component';
-import { TrafficBackCardComponent } from './traffic-reveal-card/back-side/traffic-back-card.component';
-import { TrafficBarComponent } from './traffic-reveal-card/front-side/traffic-bar/traffic-bar.component';
-import { TrafficCardsHeaderComponent } from './traffic-reveal-card/traffic-cards-header/traffic-cards-header.component';
-import { ChartPanelSummaryComponent } from './charts-panel/chart-panel-summary/chart-panel-summary.component';
-import { ChartPanelHeaderComponent } from './charts-panel/chart-panel-header/chart-panel-header.component';
-import { ECommerceLegendChartComponent } from './legend-chart/legend-chart.component';
-import { OrdersChartComponent } from './charts-panel/charts/orders-chart.component';
-import { ProfitChartComponent } from './charts-panel/charts/profit-chart.component';
-import { PieChartComponent } from './charts-panel/charts/pie-chart.component';
-import { OperatorChartsPanelComponent } from './charts-panel/charts-panel.component';
-import { TemperatureDraggerComponent } from './temperature/temperature-dragger/temperature-dragger.component';
-import { TemperatureComponent } from './temperature/temperature.component';
-import { MachineChartComponent } from './machine-fuel-bar-chart/machine-chart.component';
 
 @NgModule({
   imports: [
@@ -62,24 +45,6 @@ import { MachineChartComponent } from './machine-fuel-bar-chart/machine-chart.co
   ],
   declarations: [
     AdministrativeComponent,
-    OrdersChartComponent,
-    ProfitChartComponent,
-    PieChartComponent,
-    OperatorChartsPanelComponent,
-    ECommerceLegendChartComponent,
-    StatusCardComponent,
-    TrafficRevealCardComponent,
-
-    TrafficBarChartComponent,
-    TrafficFrontCardComponent,
-    TrafficBackCardComponent,
-    TrafficBarComponent,
-    TrafficCardsHeaderComponent,
-    ChartPanelSummaryComponent,
-    ChartPanelHeaderComponent,
-    TemperatureDraggerComponent,
-    TemperatureComponent,
-    MachineChartComponent,
   ],
   providers: [
 
